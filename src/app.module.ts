9e1001c13187a0b1672a60895ceb3d068560297c
Marius-P1 <marius.pain@epitech.eu>
2024-12-02 15:29:32 +0100

    :zap: Integrate PrismaModule and PrismaService into AppModule and AppService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  // Import the PrismaModule to use the PrismaService in the AppService
+  imports: [PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
