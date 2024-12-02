21f1b671985116d559ef1a03882d375ac0657e0b
Marius-P1 <marius.pain@epitech.eu>
2024-12-02 20:12:37 +0100

    :card_file_box: Update User model to include surname and timestamps for createdAt and updatedAt

--- prisma/migrations/20241202190643_init/migration.sql
+++ prisma/migrations/20241202190643_init/migration.sql
@@ -2,7 +2,10 @@
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
-    "name" TEXT,
+    "name" TEXT NOT NULL,
+    "surname" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
