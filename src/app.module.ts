e9c7acf9217dace40b092c27cb8d6d2e6f7dde59
Marius-P1 <marius.pain@epitech.eu>
2024-12-03 15:08:30 +0100

    :sparkles: Add AuthModule to AppModule and enable global validation pipes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { PrismaModule } from './providers/prisma/prisma.module';
 import { UsersModule } from './modules/users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { FirebaseModule } from './providers/firebase/firebase.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   // Import the PrismaModule to use the PrismaService in the AppService
-  imports: [PrismaModule, UsersModule, ConfigModule.forRoot({ cache: true }), FirebaseModule],
+  imports: [PrismaModule, UsersModule, ConfigModule.forRoot({ cache: true }), FirebaseModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
