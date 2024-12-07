f2cd610f22083304dc407e90853d1837033115ea
Marius-P1 <marius.pain@epitech.eu>
2024-12-08 00:29:25 +0100

    :memo: Add Swagger API documentation for AppController with response example

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Test')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiResponse({ status: 200, description: 'Returns Hello World!', schema: { example: 'Hello World!' }})
   getHello(): string {
     return this.appService.getHello();
   }
