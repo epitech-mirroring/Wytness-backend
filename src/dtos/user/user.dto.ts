dd4736132934de40a5b85e9117db3cbdf179256e
Marius-P1 <marius.pain@epitech.eu>
2024-12-08 00:28:53 +0100

    :memo: Add ApiProperty decorators to AuthDto and UserDto for improved API documentation

--- src/dtos/user/user.dto.ts
+++ src/dtos/user/user.dto.ts
@@ -1,13 +1,26 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UserDto {
+    @ApiProperty({
+        description: 'The email of the User',
+        example: 'usermail@site.com'
+    })
     @IsEmail()
     email: string;
 
+    @ApiProperty({
+        description: 'The name of the User',
+        example: 'John'
+    })
     @IsString()
     @IsNotEmpty()
     name: string;
 
+    @ApiProperty({
+        description: 'The surname of the User',
+        example: 'Doe'
+    })
     @IsString()
     @IsNotEmpty()
     surname: string;
