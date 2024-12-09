a89f71f904ec3f67b04f12f9c9b834010c0773a8
Marius-P1 <marius.pain@epitech.eu>
2024-12-10 00:46:43 +0100

    :sparkles: Integrate SpotifyModule and configure AuthMiddleware for Spotify routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './providers/prisma/prisma.module';
@@ -6,11 +6,19 @@ import { UsersModule } from './modules/users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { FirebaseModule } from './providers/firebase/firebase.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { SpotifyModule } from './modules/spotify/spotify.module';
+import { AuthMiddleware } from './modules/auth/auth.middleware';
 
 @Module({
   // Import the PrismaModule to use the PrismaService in the AppService
-  imports: [PrismaModule, UsersModule, ConfigModule.forRoot({ cache: true }), FirebaseModule, AuthModule],
+  imports: [PrismaModule, UsersModule, ConfigModule.forRoot({ cache: true }), FirebaseModule, AuthModule, SpotifyModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes('spotify/login', 'spotify/disconnect');
+  }
+}
