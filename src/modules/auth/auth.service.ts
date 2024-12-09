83c34a5046537eeaf9680be86040eea296ccffff
6im0n <simon.gl3135@orange.fr>
2024-12-09 23:39:19 +0100

    :pencil2: Fix the typo on auth service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,54 +1,93 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { FirebaseService } from '../../providers/firebase/firebase.service';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@firebase/auth'
-
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from '@firebase/auth';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private usersService: UsersService,
-        private firebaseService: FirebaseService,
-    ) {}
+  constructor(
+    private usersService: UsersService,
+    private firebaseService: FirebaseService,
+  ) {}
 
-    async validateToken(token: string): Promise<Boolean> {
-        const decodedToken = await this.firebaseService.getApp().auth().verifyIdToken(token).then((decodedToken) => {
-            return decodedToken;
-        }).catch((error) => {
-            return null;
-        });
-        if (decodedToken) {
-            return true;
-        }
-        return false;
+  async validateToken(token: string): Promise<boolean> {
+    const decodedToken = await this.firebaseService
+      .getApp()
+      .auth()
+      .verifyIdToken(token)
+      .then((decodedToken) => {
+        return decodedToken;
+      })
+      .catch((error) => {
+        return null;
+      });
+    if (decodedToken) {
+      return true;
     }
+    return false;
+  }
 
-    async login(email: string, password: string): Promise<{token: string} | {error: string}> {
-        const user = await signInWithEmailAndPassword(this.firebaseService.getAuth(), email, password).then((userCredential) => {
-            return userCredential.user;
-        }).catch((error) => {
-            return error.message;
-        });
-        if (typeof user === 'object') {
-            return {token: await this.firebaseService.getApp().auth().createCustomToken(user.uid)};
-        }
-        return {error: user};
+  async login(
+    email: string,
+    password: string,
+  ): Promise<{ token: string } | { error: string }> {
+    const user = await signInWithEmailAndPassword(
+      this.firebaseService.getAuth(),
+      email,
+      password,
+    )
+      .then((userCredential) => {
+        return userCredential.user;
+      })
+      .catch((error) => {
+        return error.message;
+      });
+    if (typeof user === 'object') {
+      return {
+        token: await this.firebaseService
+          .getApp()
+          .auth()
+          .createCustomToken(user.uid),
+      };
     }
+    return { error: user };
+  }
 
-    async register(email: string, password: string, name: string, surname: string): Promise<{token: string} | {error: string}> {
-        const user = await createUserWithEmailAndPassword(this.firebaseService.getAuth(), email, password).then((userCredential) => {
-            return userCredential.user;
-        }).catch((error) => {
-            return error.message;
-        });
-        if (typeof user === 'string') {
-            return {error: user};
-        }
-        const userExists = await this.usersService.getUserByEmail(email);
-        if (userExists) {
-            return {error: 'PostgreSQL: Error (user already exists with that email)'};
-        }
-        await this.usersService.createUser(email, name, surname);
-        return {token: await this.firebaseService.getApp().auth().createCustomToken(user.uid)};
+  async register(
+    email: string,
+    password: string,
+    name: string,
+    surname: string,
+  ): Promise<{ token: string } | { error: string }> {
+    const user = await createUserWithEmailAndPassword(
+      this.firebaseService.getAuth(),
+      email,
+      password,
+    )
+      .then((userCredential) => {
+        return userCredential.user;
+      })
+      .catch((error) => {
+        return error.message;
+      });
+    if (typeof user === 'string') {
+      return { error: user };
     }
+    const userExists = await this.usersService.getUserByEmail(email);
+    if (userExists) {
+      return {
+        error: 'PostgreSQL: Error (user already exists with that email)',
+      };
+    }
+    await this.usersService.createUser(email, name, surname);
+    return {
+      token: await this.firebaseService
+        .getApp()
+        .auth()
+        .createCustomToken(user.uid),
+    };
+  }
 }
