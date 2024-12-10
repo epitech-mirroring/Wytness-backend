351535a2dc39a5263ced1b8549b695ce1cec898d
Marius-P1 <marius.pain@epitech.eu>
2024-12-10 01:19:03 +0100

    :memo: Update SpotifyController with Swagger documentation

--- src/modules/spotify/spotify.controller.ts
+++ src/modules/spotify/spotify.controller.ts
@@ -1,49 +1,62 @@
-import { Controller, Get, Redirect, UnauthorizedException, Req, Body, Param } from '@nestjs/common';
+import { Controller, Get, Redirect, UnauthorizedException, Req, Res } from '@nestjs/common';
 import { Request } from 'express';
 import { SpotifyService } from './spotify.service';
 import { UsersService } from '../users/users.service';
+import { ApiResponse, ApiTags, ApiBody, ApiHeader, ApiQuery } from '@nestjs/swagger';
 
+
+@ApiTags('spotify')
 @Controller('spotify')
 export class SpotifyController {
     constructor(private readonly spotifyService: SpotifyService,
                 private readonly usersService: UsersService) {}
 
     @Get('login')
-    @Redirect('http:://localhost:3000', 301)
-    async login(@Req() request: Request) {
+    @ApiHeader({
+        name: 'Authorization',
+        description: 'Bearer token'
+    })
+    @ApiQuery({
+        name: 'link',
+        description: 'Link to redirect to after login to Spotify',
+    })
+    @ApiResponse({ status: 200, description: 'Redirect to Spotify auth page', schema: { example: { url: 'string' } }})
+    @ApiResponse({ status: 401, description: 'Unauthorized', schema: { example: { statusCode: 401, error: 'string' } }})
+    async login(@Req() request: Request, @Res() response) {
         const email = request['user_email'];
         const link = request.query.link as string;
         if (!email) {
-            return new UnauthorizedException('No email found');
+            throw new UnauthorizedException('No email found');
         }
         if (!link) {
-            return new UnauthorizedException('No link found');
+            throw new UnauthorizedException('No link found');
         }
         if (await this.usersService.getUserCredentialByServiceType(email, 'SPOTIFY')) {
-            return new UnauthorizedException('User already has Spotify credentials');
+            throw new UnauthorizedException('User already has Spotify credentials');
         }
         if (!('authorization' in request.headers)) {
-            return new UnauthorizedException('No authorization header found');
+            throw new UnauthorizedException('No authorization header found');
         }
         const user_token = request.headers.authorization.split(' ')[1];
         if (!user_token) {
-            return new UnauthorizedException('No user token found');
+            throw new UnauthorizedException('No user token found');
         }
         const url = this.spotifyService.login(user_token, link);
-        return { url: url };
+        response.status(301).redirect(url);
     }
 
     @Get('callback')
-    @Redirect('http:://localhost:3000', 301)
-    async callback(@Req() request: Request) {
+    @ApiResponse({ status: 200, description: 'Redirect to link', schema: { example: { url: 'string' } }})
+    @ApiResponse({ status: 401, description: 'Unauthorized', schema: { example: { statusCode: 401, error: 'string' } }})
+    async callback(@Req() request: Request, @Res() response) {
         if ('error' in request.query) {
-            return new UnauthorizedException(request.query.error);
+            throw new UnauthorizedException(request.query.error);
         }
         if (!('code' in request.query)) {
-            return new UnauthorizedException('No code in query');
+            throw new UnauthorizedException('No code in query');
         }
         if (!('state' in request.query)) {
-            return new UnauthorizedException('Missing state in query');
+            throw new UnauthorizedException('Missing state in query');
         }
         const state_json = JSON.parse(Buffer.from(request.query.state as string, 'base64').toString());
         const state = state_json['user_token'];
@@ -51,19 +64,25 @@ export class SpotifyController {
         const code = request.query.code as string;
         const success = await this.spotifyService.callback(code, state);
         if (success) {
-            return { url: link };
+            response.status(301).redirect(link);
         }
-        return new UnauthorizedException('Error while processing callback of Spotify');
+        throw new UnauthorizedException('Error while processing callback of Spotify');
     }
 
     @Get('disconnect')
+    @ApiHeader({
+        name: 'Authorization',
+        description: 'Bearer token'
+    })
+    @ApiResponse({ status: 200, description: 'Success', schema: { example: 'Success' }})
+    @ApiResponse({ status: 401, description: 'Unauthorized', schema: { example: { statusCode: 401, error: 'string' } }})
     async disconnect(@Req() request: Request) {
         const email = request['user_email'];
         if (!email) {
-            return new UnauthorizedException('No email found');
+            throw new UnauthorizedException('No email found');
         }
         if (!(await this.spotifyService.unsubscribe(email))) {
-            return new UnauthorizedException('You are not subscribed to Spotify');
+            throw new UnauthorizedException('You are not subscribed to Spotify');
         }
         return 'Success';
     }
